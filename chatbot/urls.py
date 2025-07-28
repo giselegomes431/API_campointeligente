2319c3d8a59159d81963f0c7ff1d5dc8145996e1
Abimael Cristovao <abimael.servicos12dt@gmail.com>
2025-07-28 09:04:08 -0300

    Subindo alterações para rodar chatbot

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,7 @@ from .views import webhook_view, webchat_view
 
 urlpatterns = [
     # Rota para o webhook do WhatsApp
-    path('webhook/', webhook_view, name='webhook'),
+    path('webhook', webhook_view, name='webhook'),
     
     # Nova rota para o webchat do site
     path('webchat/', webchat_view, name='webchat'),
